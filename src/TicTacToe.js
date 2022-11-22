4ffe779b5a694145877b6158444b4474cda43e64
Matheus <matheusroca11@gmail.com>
2022-11-22 02:39:50 -0300

    turning winnable

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './TicTacToe.css';
 
 function TicTacToe() {
@@ -7,9 +7,16 @@ function TicTacToe() {
   const [board, setBoard] = useState(emptyBoard);
   // Definir atual jogador
   const [currentPlayer, setCurrentPlayer] = useState("X");
+  // Verificar se houve ganhador
+  useEffect(() => {
+    checkWinner()
+  }, [board]);
 
   // Função para retornar a casa do clique
   const handleCellClick = (index) => {
+    // Não permitir jogar 2 vezes na mesma casa
+    if (board[index] != "") return null;
+
     // Retornar o board com o clique atualizado
     // O classname também muda para "cell X" ou "cell O"
     setBoard(
@@ -19,6 +26,26 @@ function TicTacToe() {
 
     // Trocar atual jogador
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  const checkWinner = () => {
+    const possibleWaysToWin = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+      
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+      
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ];
+
+    possibleWaysToWin.forEach(cells => {
+      if (cells.every(cell => cell === "O")) alert("O venceu!")
+      if (cells.every(cell => cell === "X")) alert("X venceu!")
+    });
   };
 
   return (
